1ca0093aef4ba11374dacd6e8b9c599cab822e75
Thales <th4les.castro@gmail.com>
2018-09-30 19:43:21 -0300

    atualizando form_cep

--- form_cep.py
+++ form_cep.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from consulta_cep import *
 
 class Form_cep:
@@ -27,51 +27,53 @@ class Form_cep:
            estado['bg'] = '#F3F781'
 
 
-        janela = tk.Tk()
+        janela = Tk()
         janela['bg'] = '#F2F2F2'
         janela.title("Endereco")
 
-        lbJanela = tk.Label(janela, text = 'Cadastro de Endereco')
+        lbJanela = Label(janela, text = 'Cadastro de Endereco')
         lbJanela['bg'] = '#F2F2F2'
-        lbJanela.place(x = 170, y = 25)
+        #lbJanela.place(x = 170, y = 25)
+        lbJanela.pack(side=TOP)
 
-        cep = tk.Entry(janela)
+
+        cep = Entry(janela)
         cep.place(x= 100, y=50,width=100)
-        lbCep = tk.Label(janela, text='CEP: ')
+        lbCep = Label(janela, text='CEP: ')
         lbCep['bg'] = '#F2F2F2'
         lbCep.place(x = 25, y=50)
 
-        logadouro = tk.Entry(janela)
+        logadouro = Entry(janela)
         logadouro.place(x= 100, y=100,width=300)
-        lbLog = tk.Label(janela, text='Logadouro: ')
+        lbLog = Label(janela, text='Logadouro: ')
         lbLog['bg'] = '#F2F2F2'
         lbLog.place(x = 25, y=100)
 
-        bairro = tk.Entry(janela)
+        bairro = Entry(janela)
         bairro.place(x= 100, y=150,width=300)
-        lbBairro = tk.Label(janela, text='Bairro: ')
+        lbBairro = Label(janela, text='Bairro: ')
         lbBairro['bg'] = '#F2F2F2'
         lbBairro.place(x = 25, y=150)
 
-        cidade = tk.Entry(janela)
+        cidade = Entry(janela)
         cidade.place(x= 100, y=200,width=300)
-        lbCity = tk.Label(janela, text='Cidade: ')
+        lbCity = Label(janela, text='Cidade: ')
         lbCity['bg'] = '#F2F2F2'
         lbCity.place(x = 25, y=200)
 
-        estado = tk.Entry(janela)
+        estado = Entry(janela)
         estado.place(x= 100, y=250,width=300)
-        lbState = tk.Label(janela, text='Estado: ')
+        lbState = Label(janela, text='Estado: ')
         lbState['bg'] = '#F2F2F2'
         lbState.place(x = 25, y=250)
 
-        btProcuraCep = tk.Button(janela, text = "Procurar",command = procura_cep )
+        btProcuraCep = Button(janela, text = "Procurar",command = procura_cep )
         btProcuraCep.place(x = 170, y=280)
 
-        btConfirmar = tk.Button(janela, text = "Confirmar")
+        btConfirmar = Button(janela, text = "Confirmar")
         btConfirmar.place(x = 250, y=280)
 
-        btVoltar = tk.Button(janela,text = "Voltar")
+        btVoltar = Button(janela,text = "Voltar")
         btVoltar.place(x = 340, y=280)
 
         janela.geometry('450x350')

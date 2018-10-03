ff74d5bf2eb7afe34be5caba9c237867b7774667
Thales <th4les.castro@gmail.com>
2018-10-03 12:55:31 -0300

    novos forms

---
+++ form_principal.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class Form_principal:
+
+    def Janela_inicial(self):
+
+        janela = Tk()
+        janela['bg'] = '#F2F2F2'
+        janela.title('Pizzaria')
+
+        janela.geometry('700x700')
+
+        janela.mainloop()
